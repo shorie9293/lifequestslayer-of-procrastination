c9db1efa20ae19365597af2a898fcd26af543fba
Horie Shunta <horie.shunta@gmail.com>
2026-05-31 06:53:14 +0900

    fix: resolve analyze error - add missing import for JobSkill in overview_screen.dart

--- lib/features/overview/presentation/screens/overview_screen.dart
+++ lib/features/overview/presentation/screens/overview_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:rpg_todo/domain/models/player.dart';
 import 'package:rpg_todo/domain/models/task.dart';
 import 'package:rpg_todo/features/guild/viewmodels/task_view_model.dart';
 import 'package:rpg_todo/features/overview/domain/overview_service.dart';
